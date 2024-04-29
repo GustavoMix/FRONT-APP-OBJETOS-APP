3c8ed7d7cea56f5a028143bec79badcabc91fbba
Grover Gustavo Tintaya Apaza <gustavotna1@gmail.com>
2024-04-28 23:24:33 -0400

    cambios

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-objetos';
+
+  isNavbarCollapsed = false;
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
 }
