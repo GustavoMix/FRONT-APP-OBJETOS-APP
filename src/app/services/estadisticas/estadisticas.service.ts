fe8628a205434459dabf80809665407822ff54fa
Grover Gustavo Tintaya Apaza <gustavotna1@gmail.com>
2024-11-25 23:26:47 -0400

    AJSUTES ESTILOS

--- src/app/services/estadisticas/estadisticas.service.ts
+++ src/app/services/estadisticas/estadisticas.service.ts
@@ -1,3 +1,25 @@
+// import { HttpClient } from '@angular/common/http';
+// import { Injectable } from '@angular/core';
+// import { lastValueFrom } from 'rxjs';
+// import { environment } from 'src/environments/environments';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class EstadisticasService {
+//   urlBack = environment.rutaApiBackend + '/api';
+//   constructor(private http: HttpClient) { 
+
+//   }
+
+//   async estadisticas() {
+//     let url = `${this.urlBack}/objetos/estadisticas`;
+//     return await lastValueFrom(this.http.get<any>(url));
+//   }
+
+// }
+
+
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
@@ -8,13 +30,27 @@ import { environment } from 'src/environments/environments';
 })
 export class EstadisticasService {
   urlBack = environment.rutaApiBackend + '/api';
-  constructor(private http: HttpClient) { 
-
-  }
+  
+  constructor(private http: HttpClient) { }
 
   async estadisticas() {
-    let url = `${this.urlBack}/objetos/estadisticas`;
-    return await lastValueFrom(this.http.get<any>(url));
-  }
+    const simulate = true; // Cambia a `false` cuando quieras usar el servicio real.
 
+    if (simulate) {
+      // Datos simulados
+      return Promise.resolve({
+        codigoRespuesta: "2000",
+        mensaje: "Datos simulados correctamente",
+        data: [
+          { tipo: "Usuarios activos", valor: 120 },
+          { tipo: "Sesiones activas", valor: 75 },
+          { tipo: "Objetos registrados", valor: 50 }
+        ]
+      });
+    } else {
+      // Llamada real al servicio
+      const url = `${this.urlBack}/objetos/estadisticas`;
+      return await lastValueFrom(this.http.get<any>(url));
+    }
+  }
 }
